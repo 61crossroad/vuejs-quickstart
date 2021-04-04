8defc29a772b70003b0ea1c94c4fe1ee9b652614
iMac21.5-2012 <61crossroad@gmail.com>
2021-04-04 12:04:31 +0900

    Event Bus

---
+++ contactsapp/src/Constant.js
@@ -0,0 +1,14 @@
+export default {
+    // For mutations and actions
+    ADD_CONTACT_FORM : "addContactForm",
+    CANCEL_FORM : "cancelForm",
+    EDIT_CONTACT_FORM : "editContactForm",
+    EDIT_PHOTO_FORM : "editPhotoForm",
+    FETCH_CONTACTS : "fetchContacts",
+
+    // For actions only
+    ADD_CONTACT : "addContact",
+    UPDATE_CONTACT : "updateContact",
+    UPDATE_PHOTO : "updatePhoto",
+    DELETE_CONTACT : "deleteContact"
+}
