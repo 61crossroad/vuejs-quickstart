938a53e7bc2dfc848688f0c6db3edda44e4e7de9
iMac21.5-2012 <61crossroad@gmail.com>
2021-04-04 19:33:04 +0900

    add route

---
+++ contactsapp_route/src/App.vue
@@ -0,0 +1,194 @@
+<template>
+  <div id="container">
+    <div class="page-header">
+      <h1 class="text-center">연락처 관리 애플리케이션</h1>
+      <!-- <p>(Dynamic Component + EventBus + Axios)</p> -->
+      <!-- <p>(Dynamic Component + Vuex + Axios)</p> -->
+      <p>(Vue-router + Vuex + Axios)</p>
+      <div class="btn-group">
+        <router-link to="/home" class="btn btn-info menu">Home</router-link>
+        <router-link to="/about" class="btn btn-info menu">About</router-link>
+        <router-link to="/contacts" class="btn btn-info menu">Contacts</router-link>
+      </div>
+    </div>
+    <!-- <component :is="currentView" :contact="contact"></component>
+    <contactList :contactlist="contactlist"></contactList> -->
+    <!-- <component :is="currentView"></component>
+    <contactList></contactList> -->
+    <router-view></router-view>
+    <loading v-show="isloading"></loading>
+  </div>
+</template>
+
+<script>
+// import ContactList from './components/ContactList';
+
+// import AddContact from './components/AddContact';
+// import UpdateContact from './components/UpdateContact';
+
+// import ContactForm from './components/ContactForm';
+// import UpdatePhoto from './components/UpdatePhoto';
+// import CONF from './Config.js';
+// import eventBus from './EventBus.js';
+
+import { mapState } from 'vuex';
+import Loading from './components/Loading';
+
+export default {
+  name: 'app',
+  components : { Loading },
+  computed : mapState([ 'isloading' ])
+  // components: { ContactList, AddContact, UpdateContact, UpdatePhoto },
+
+  /*
+  components : { ContactList, ContactForm, UpdatePhoto },
+  computed : mapState([ 'currentView' ])
+  */
+
+  /*
+  data : function() {
+    return {
+      currentView : null,
+      contact : { no:0, name:'', tel:'', address:'', photo:'' },
+      contactlist : {
+        pageno:1, pagesize: CONF.PAGESIZE, totalcount:0, contacts:[]
+      }
+    }
+  },
+  mounted : function() {
+    this.fetchContacts();
+    
+    eventBus.$on("cancel", () => {
+      this.currentView = null;
+    });
+    eventBus.$on("addSubmit", (contact) => {
+      this.currentView = null;
+      this.addContact(contact);
+    });
+    eventBus.$on("updateSubmit", (contact) => {
+      this.currentView = null;
+      this.updateContact(contact);
+    });
+    eventBus.$on("addContactForm", () => {
+      this.currentView = 'addContact';
+    });
+    eventBus.$on("editContactForm", (no) => {
+      this.fetchContactOne(no);
+      this.currentView = 'updateContact';
+    });
+    eventBus.$on("deleteContact", (no) => {
+      this.deleteContact(no);
+    });
+    eventBus.$on("editPhoto", (no) => {
+      this.fetchContactOne(no);
+      this.currentView = 'updatePhoto';
+    });
+    eventBus.$on("updatePhoto", (no, file) => {
+      if (typeof file != 'undefined') {
+        this.updatePhoto(no, file);
+      }
+      this.currentView = null;
+    });
+    eventBus.$on("pageChanged", (page) => {
+      this.pageChanged(page);
+    });
+  },
+  methods : {
+    pageChanged : function(page) {
+      this.contactlist.pageno = page;
+      this.fetchContacts();
+    },
+    fetchContacts : function() {
+      this.$axios.get(CONF.FETCH, {
+        params : {
+          pageno:this.contactlist.pageno,
+          pagesize:this.contactlist.pagesize
+        }
+      })
+      .then((response) => {
+        this.contactlist = response.data;
+      })
+      .catch((ex) => {
+        console.log('fetchContacts failed', ex);
+        this.contactlist.contacts = [];
+      })
+    },
+    addContact : function(contact) {
+      this.$axios.post(CONF.ADD, contact)
+      .then((response) => {
+        if (response.data.status === "success") {
+          this.contactlist.pageno = 1;
+          this.fetchContacts();
+        } else {
+          console.log('연락처 추가 실패 : ' + response.data.message);
+        }
+      })
+      .catch((ex) => {
+        console.log('addContact failed : ', ex);
+      })
+    },
+    updateContact : function(contact) {
+      this.$axios.put(CONF.UPDATE.replace("${no}", contact.no), contact)
+      .then((response) => {
+        if (response.data.status === "success") {
+          this.fetchContacts();
+        } else {
+          console.log('연락처 변경 실패 : ' + response.data.message);
+        }
+      })
+      .catch((ex) => {
+        console.log('updateContact failed : ', ex);
+      })
+    },
+    fetchContactOne : function(no) {
+      this.$axios.get(CONF.FETCH_ONE.replace("${no}", no))
+      .then((response) => {
+        this.contact = response.data;
+      })
+      .catch((ex) => {
+        console.log('fetchContactOne failed', ex);
+      })
+    },
+    deleteContact : function(no) {
+      this.$axios.delete(CONF.DELETE.replace("${no}", no))
+      .then((response) => {
+        if (response.data.status === "success") {
+          this.fetchContacts();
+        } else {
+          console.log('연락처 삭제 실패 : ' + response.data.message);
+        }
+      })
+      .catch((ex) => {
+        console.log('delete failed', ex);
+      })
+    },
+    updatePhoto : function(no, file) {
+      var data = new FormData();
+      data.append('photo', file);
+      this.$axios.post(CONF.UPDATE_PHOTO.replace("${no}", no), data)
+      .then((response) => {
+        if (response.data.status === "success") {
+          this.fetchContacts();
+        } else {
+          console.log('연락처 사진 변경 실패 : ' + response.data.message);
+        }
+      })
+      .catch((ex) => {
+        console.log('updatePhoto failed', ex);
+      });
+    }
+  }
+  */
+}
+</script>
+
+<style scoped>
+#container {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ contactsapp_route/src/Constant.js
@@ -0,0 +1,19 @@
+export default {
+    // For mutations and actions
+    /*
+    ADD_CONTACT_FORM : "addContactForm",
+    CANCEL_FORM : "cancelForm",
+    EDIT_CONTACT_FORM : "editContactForm",
+    EDIT_PHOTO_FORM : "editPhotoForm",
+    */
+    FETCH_CONTACTS : "fetchContacts",
+    FETCH_CONTACT_ONE : "fetchContactOne",
+    INITIALIZE_CONTACT_ONE : "initializeContactOne",
+    CHANGE_ISLOADING : "changeIsLoading",
+
+    // For actions only
+    ADD_CONTACT : "addContact",
+    UPDATE_CONTACT : "updateContact",
+    UPDATE_PHOTO : "updatePhoto",
+    DELETE_CONTACT : "deleteContact"
+}
