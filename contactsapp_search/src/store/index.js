7fa9fbdd049873dbd6607e8a242acd2f662e02f1
iMac21.5-2012 <61crossroad@gmail.com>
2021-04-07 21:56:33 +0900

    Es6 Promise, Vuex, Axios

---
+++ contactsapp_search/src/store/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Constant from '../Constant';
+import axios from 'axios';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        contacts : []
+    },
+    mutations: {
+        [Constant.SEARCH_CONTACT] : (state, payload) => {
+            state.contacts = payload.contacts;
+        }
+    },
+    actions: {
+        /*
+        [Constant.SEARCH_CONTACT] : (store, payload) => {
+            axios.get(Constant.BASE_URL + payload.name)
+            .then((response) => {
+                store.commit(Constant.SEARCH_CONTACT, { contacts: response.data });
+            })
+        }
+        */
+        async [Constant.SEARCH_CONTACT](store, payload) {
+            var response = await axios.get(Constant.BASE_URL + payload.name);
+            store.commit(Constant.SEARCH_CONTACT, { contacts: response.data });
+        }
+    }
+})
+
+export default store;
